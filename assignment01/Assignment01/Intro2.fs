35573ea731453b0399a27e54c19f312b673e3e91
Thomas Bavnhøj <thomas.bavnhoj@gmail.com>
2023-08-31 12:09:13 +0200

    Add 1.2

--- assignment01/Assignment01/Intro2.fs
+++ assignment01/Assignment01/Intro2.fs
@@ -1,4 +1,4 @@
-module Assignment01.Intro2
+module Intro2 
 
 (* Programming language concepts for software developers, 2010-08-28 *)
 
@@ -48,11 +48,16 @@ let e7 = Prim("==", CstI 23, CstI 26)
 let rec eval e (env: (string * int) list) : int =
     match e with
     | CstI i -> i
+    | If(condition, e1, e2) ->
+        if (eval condition env) <> 0 then
+            (eval e1 env)
+        else
+            (eval e2 env)
     | Var x -> lookup env x
     | Prim("+", e1, e2) -> eval e1 env + eval e2 env
     | Prim("*", e1, e2) -> eval e1 env * eval e2 env
     | Prim("-", e1, e2) -> eval e1 env - eval e2 env
-    | Prim("max", e1, e2) -> max (eval e1 env) (eval e2 env)
+    | Prim("max", e1, e2) -> max (eval e1 env) (eval e2 env) // 1.1.1 and down
     | Prim("min", e1, e2) -> min (eval e1 env) (eval e2 env)
     | Prim("==", e1, e2) -> if (eval e1 env) = (eval e2 env) then 1 else 0
     | Prim _ -> failwith "unknown primitive"
@@ -62,6 +67,7 @@ let e2v1 = eval e2 env
 let e2v2 = eval e2 [ ("a", 314) ]
 let e3v = eval e3 env
 
+//1.1.2
 let maxTest = eval e4 env
 
 let minTest = eval e5 env
@@ -69,6 +75,7 @@ let minTest = eval e5 env
 let eqTestTrue = eval e6 env
 let eqTestFalse = eval e7 env
 
+// 1.1.3
 let rec evalPrim e (env: (string * int) list) : int =
     match e with
     | CstI i -> i
@@ -89,3 +96,32 @@ let rec evalPrim e (env: (string * int) list) : int =
         | "min" -> min i1 i2
         | "==" -> if i1 = i2 then 1 else 0
         | _ -> failwith "unknown primitive"
+
+// 1.2.1
+type aexpr =
+    | CstI of int
+    | Var of string
+    | Add of aexpr * aexpr
+    | Sub of aexpr * aexpr
+    | Mul of aexpr * aexpr
+
+// 1.2.2
+let ae1 = Sub(Var "v",Add(Var "w", Var "z")) // v-(w+z)
+
+let ae2 = Mul(CstI 2,Sub(Var "v",Add(Var "w",Var "z"))) // 2 * (v-(w+z))
+
+let ae3 = Add(Var "x",Add(Var "y", Add(Var "z",Var "v"))) // x+y+z+v
+
+// 1.2.3
+let rec fmt =
+    function
+    | CstI e1 -> e1.ToString()
+    | Var e -> e
+    | Add (e1,e2) -> $"({fmt e1} + {fmt e2})"
+    | Sub (e1,e2) -> $"({fmt e1} - {fmt e2})"
+    | Mul (e1,e2) -> $"({fmt e1} * {fmt e2})"
+
+let formatted = fmt ae3
+    
+    
+    
