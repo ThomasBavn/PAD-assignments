b29ef42fda2a0a2f179a5934d48e747c68a860f6
Philip Mørch <mrphil2105@gmail.com>
2023-10-26 12:11:11 +0200

    Implement bytecode decoder

---
+++ assignment07/Assignment07/BytecodeDecorator/Program.cs
@@ -0,0 +1,117 @@
+﻿public static class Program
+{
+    private static readonly IReadOnlyDictionary<int, string> _lookupTable = new Dictionary<int, string>()
+    {
+        { 0, "CSTI" },
+        { 1, "ADD" },
+        { 2, "SUB" },
+        { 3, "MUL" },
+        { 4, "DIV" },
+        { 5, "MOD" },
+        { 6, "EQ" },
+        { 7, "LT" },
+        { 8, "NOT" },
+        { 9, "DUP" },
+        { 10, "SWAP" },
+        { 11, "LDI" },
+        { 12, "STI" },
+        { 13, "GETBP" },
+        { 14, "GETSP" },
+        { 15, "INCSP" },
+        { 16, "GOTO" },
+        { 17, "IFZERO" },
+        { 18, "IFNZRO" },
+        { 19, "CALL" },
+        { 20, "TCALL" },
+        { 21, "RET" },
+        { 22, "PRINTI" },
+        { 23, "PRINTC" },
+        { 24, "LDARGS" },
+        { 25, "STOP" }
+    };
+
+    public static void Main(string[] args)
+    {
+        //var fileName = args[0];
+        var fileName = "ex3.out";
+        var line = File.ReadAllText(fileName);
+        var numbers = line.Split(' ').Select(int.Parse).ToList();
+        var enumerator = numbers.GetEnumerator();
+        var output = DecodeInstruction(enumerator, new List<string>());
+        File.WriteAllText(Path.GetFileNameWithoutExtension(fileName) + ".decoded", output);
+    }
+
+    private static string DecodeInstruction(IEnumerator<int> numbers, List<string> instructions)
+    {
+        if (!numbers.MoveNext())
+        {
+            return string.Join(Environment.NewLine, instructions);
+        }
+
+        var instruction = (numbers.Current + ": ").PadLeft(4);
+
+        instruction += numbers.Current switch
+        {
+            0 => "CSTI",
+            1 => "ADD",
+            2 => "SUB",
+            3 => "MUL",
+            4 => "DIV",
+            5 => "MOD",
+            6 => "EQ",
+            7 => "LT",
+            8 => "NOT",
+            9 => "DUP",
+            10 => "SWAP",
+            11 => "LDI",
+            12 => "STI",
+            13 => "GETBP",
+            14 => "GETSP",
+            15 => "INCSP",
+            16 => "GOTO",
+            17 => "IFZERO",
+            18 => "IFNZRO",
+            19 => "CALL",
+            20 => "TCALL",
+            21 => "RET",
+            22 => "PRINTI",
+            23 => "PRINTC",
+            24 => "LDARGS",
+            25 => "STOP"
+        };
+
+        switch (numbers.Current)
+        {
+            case 0:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 21:
+                numbers.MoveNext();
+                instruction += $" {numbers.Current}";
+
+                break;
+            case 19:
+                numbers.MoveNext();
+                instruction += $" {numbers.Current}";
+                numbers.MoveNext();
+                instruction += $" {numbers.Current}";
+
+                break;
+            case 20:
+                numbers.MoveNext();
+                instruction += $" {numbers.Current}";
+                numbers.MoveNext();
+                instruction += $" {numbers.Current}";
+                numbers.MoveNext();
+                instruction += $" {numbers.Current}";
+
+                break;
+        }
+
+        instructions.Add(instruction);
+
+        return DecodeInstruction(numbers, instructions);
+    }
+}
