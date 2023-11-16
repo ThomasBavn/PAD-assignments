fcfce1f9d03a459b5cadd8ff3189965cfb737687
Thomas Bavnhøj <thomas.bavnhoj@gmail.com>
2023-11-16 12:09:25 +0100

    Add len and reverse continuation

---
+++ assignment10/Assignment10/Assignment10/Answers.md
@@ -0,0 +1,26 @@
+# Assignment 10
+
+## 11.1
+
+
+### 11.1.i
+
+Implementation can be found in `Program.fs`.
+
+When run, it gives the result `3`.
+
+### 11.1.ii
+
+If we run with `lenc xs (fun v -> 2 *v)`, the result is 6 instead of 3
+
+### 11.1.iii
+
+Implementation can be found in `Program.fs`.
+When run, it gives the result `3`.
+
+The relationship between `lenc` and `leni` is that they are both tail recursive to make sure that the stack depth 
+is not unnecessarily increased.
+
+### 11.2
+
+
--- assignment10/Assignment10/Assignment10/Program.fs
+++ assignment10/Assignment10/Assignment10/Program.fs
@@ -1,2 +1,43 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+
+let rec len xs =
+    match xs with
+    | [] -> 0
+    | x::xr -> 1 + len xr;;
+    
+let rec lenc xs c =
+   match xs with
+   | [] -> c 0
+   | _::xr -> lenc xr (fun res -> c (1 + res))
+
+let exArr = [2;5;7]
+
+let normalLen = len exArr
+let cLen = lenc exArr (fun v -> 2 * v)
+
+printf "%d " cLen 
+printf "%d " normalLen
+
+let rec leni xs acc =
+    match xs with
+    | [] -> acc
+    | _::xs -> leni xs (acc+1)
+
+printf "%d \n" (leni exArr 0)
+
+let rec rev xs =
+    match xs with
+    | [] -> []
+    | x::xr -> rev xr @ [x]
+    
+let rec revc xs c =
+    match xs with
+    | [] -> c []
+    | x::xs -> revc xs (fun res -> c (res @ [x]))
+    
+    
+let cRev = revc exArr id
+let normalRev = rev exArr
+
+printf "%A " cRev
+printf "%A " normalRev
